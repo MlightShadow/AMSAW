87217230cd0d20799194fb1eb0ae4d18362af414
Chengwei_Xu <>
2020-09-01 11:45:47 +0800

    init: backend

---
+++ backend/src/main/java/com/company/project/model/Account.java
@@ -0,0 +1,191 @@
+package com.company.project.model;
+
+import java.util.Date;
+import javax.persistence.*;
+
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    private String creator;
+
+    @Column(name = "create_time")
+    private Date createTime;
+
+    /**
+     * 帐户名
+     */
+    private String name;
+
+    /**
+     * 昵称
+     */
+    @Column(name = "nick_name")
+    private String nickName;
+
+    /**
+     * 头像
+     */
+    private String avatar;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 最近登录时间
+     */
+    @Column(name = "last_login_time")
+    private Date lastLoginTime;
+
+    /**
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return is_deleted
+     */
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    /**
+     * @param isDeleted
+     */
+    public void setIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    /**
+     * @return creator
+     */
+    public String getCreator() {
+        return creator;
+    }
+
+    /**
+     * @param creator
+     */
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    /**
+     * @return create_time
+     */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * @param createTime
+     */
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    /**
+     * 获取帐户名
+     *
+     * @return name - 帐户名
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置帐户名
+     *
+     * @param name 帐户名
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取昵称
+     *
+     * @return nick_name - 昵称
+     */
+    public String getNickName() {
+        return nickName;
+    }
+
+    /**
+     * 设置昵称
+     *
+     * @param nickName 昵称
+     */
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    /**
+     * 获取头像
+     *
+     * @return avatar - 头像
+     */
+    public String getAvatar() {
+        return avatar;
+    }
+
+    /**
+     * 设置头像
+     *
+     * @param avatar 头像
+     */
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    /**
+     * 获取密码
+     *
+     * @return password - 密码
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置密码
+     *
+     * @param password 密码
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取最近登录时间
+     *
+     * @return last_login_time - 最近登录时间
+     */
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    /**
+     * 设置最近登录时间
+     *
+     * @param lastLoginTime 最近登录时间
+     */
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+}
