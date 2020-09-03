6a836b3056803cafa34b83e6626529653de1f4dd
Chengwei_Xu <rwayxu@hotmail.com>
2020-09-03 17:30:14 +0800

    update: 计算调整

--- backend/src/main/java/com/company/project/util/EncodeUtil.java
+++ backend/src/main/java/com/company/project/util/EncodeUtil.java
@@ -20,7 +20,7 @@ public class EncodeUtil {
 
     public static byte[] salt() {
         BigInteger res = new BigInteger(StringUtils.upperCase(UUID.randomUUID().toString()).replaceAll("-", "")
-                + StringUtils.upperCase(UUID.randomUUID().toString()).replaceAll("-", "").substring(0,30), 16);
+                + StringUtils.upperCase(UUID.randomUUID().toString()).replaceAll("-", "").substring(0, 30), 16);
         return res.toByteArray();
     }
 
@@ -31,12 +31,16 @@ public class EncodeUtil {
         System.arraycopy(salt, 0, mb, 0, salt.length);
         System.arraycopy(sha_pass_hash, 0, mb, salt.length, sha_pass_hash.length);
         byte[] h2 = DigestUtils.sha1Hex(mb).getBytes();
-        int int1 = h2[0] & 0xff;
-        int int2 = (h2[1] & 0xff) << 8;
-        int int3 = (h2[2] & 0xff) << 16;
-        int int4 = (h2[3] & 0xff) << 24;
-
-        BigInteger ih2 = BigInteger.valueOf(int1 | int2 | int3 | int4);
+        BigInteger ih2 = new BigInteger("0");
+        for (int offset = 0; offset < 40; offset += 4) {
+            int int1 = h2[0 + offset] & 0xff;
+            int int2 = (h2[1 + offset] & 0xff) << 8;
+            int int3 = (h2[2 + offset] & 0xff) << 16;
+            int int4 = (h2[3 + offset] & 0xff) << 24;
+            String count = String.valueOf(int1 | int2 | int3 | int4);
+            ih2.add(new BigInteger(count));
+            ih2.add(new BigInteger("1"));
+        }
 
         BigInteger g = new BigInteger("7");
         BigInteger N = new BigInteger("894B645E89E1535BBDAD5B8B290650530801B18EBFBF5E8FAB3C82872A3E9BB7", 16);
